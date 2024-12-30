3840bf9c309e1d460900777382a8a390d4f3ed37
chriskim <kim3651@purdue.edu>
2024-12-29 22:09:22 -0500

    sunday work after panda east

---
+++ oppenheimer/PersistenceController.swift
@@ -0,0 +1,27 @@
+//
+//  PersistenceController.swift
+//  oppenheimer
+//
+//  Created by Chris Kim on 12/29/24.
+//
+
+// Core Data -->
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "WorkoutModel") // .xcdatamodeld
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
